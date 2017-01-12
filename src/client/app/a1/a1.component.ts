801ea3de13ae7fc325f5f32cfc56293cc28cd759
Stanley Chen <stanley@interlo.co>
2017-01-12 02:27:18 -0800

    Puts all game state in grid class. Includes lodash library.

--- src/client/app/a1/a1.component.ts
+++ src/client/app/a1/a1.component.ts
@@ -4,15 +4,6 @@ import { Component, OnInit, } from '@angular/core';
 import { KickInstrument, SnareInstrument} from '../shared/instruments/instrument';
 import { Grid } from '../shared/grid/grid';
 
-const livePlayWithin: number = 0.3;
-
-enum State {
-  Count,
-  Demo,
-  Play,
-  Victory
-}
-
 /**
  * This class represents the lazy loaded A1Component.
  */
@@ -25,14 +16,6 @@ enum State {
 export class A1Component implements OnInit {
   gridLoop: Tone.Loop;
   noteLoop: Tone.Loop;
-  state: State;
-  nextState: State = State.Count;
-  round: number = 0;
-  beat: number = 0;
-  lastDelay: number = 0;
-  active: boolean = false;
-  inactiveRounds: number = 0;
-
   grid: Grid;
 
   /**
@@ -46,48 +29,10 @@ export class A1Component implements OnInit {
   ngOnInit() {
     this.grid = new Grid([new SnareInstrument(), new KickInstrument], 4);
 
-    this.gridLoop = new Tone.Loop((time) => {
-      if (this.grid.reachedGoal()) {
-        this.grid = new Grid([new SnareInstrument(), new KickInstrument], 4);
-        this.nextState = State.Victory;
-      } else if (this.active) {
-        this.inactiveRounds = 0;
-      } else if (this.inactiveRounds >= 3) {
-        this.nextState = State.Demo;
-      }
-      this.state = this.nextState;
-      this.active = false;
-      this.beat = 0;
-      switch(this.state) {
-        case State.Count:
-        case State.Victory:
-          this.nextState = State.Demo;
-          this.round = 0;
-
-          break;
-        case State.Demo:
-          this.nextState = State.Play;
-          this.inactiveRounds = 0;
-          break;
-        case State.Play:
-          this.round++;
-          if (!this.active) {
-            this.inactiveRounds++;
-          } else {
-            this.inactiveRounds = 0;
-          }
-      }
-    }, '1m');
+    this.gridLoop = new Tone.Loop((time) => { this.grid.onTop() }, '1m');
     this.gridLoop.start(0);
 
-    this.noteLoop = new Tone.Loop((time) => {
-      if (this.state === State.Demo) {
-        this.grid.playGoal(this.beat);
-      } else {
-        this.grid.playBeat(this.beat);
-      }
-      this.beat++;
-    }, '4n');
+    this.noteLoop = new Tone.Loop((time) => { this.grid.onBeat() }, '4n');
     this.noteLoop.start(0);
 
     Tone.Transport.start();
@@ -107,29 +52,6 @@ export class A1Component implements OnInit {
   }
 
   setBeatState(stripIndex: number, beatIndex: number) {
-    if (this.state === State.Count || this.state === State.Victory) {
-      return;
-    }
-    this.active = true;
-    this.grid.toggle(stripIndex, beatIndex, () => {
-      if (beatIndex !== this.beat - 1) {
-        this.lastDelay = 0;
-        return false;
-      }
-      this.lastDelay = this.noteLoop.progress;
-      return this.noteLoop.progress < livePlayWithin;
-    });
-  }
-
-  stateName() {
-    return State[this.state];
-  }
-
-  showCount() {
-    return this.state === State.Count || this.state === State.Victory;
-  }
-
-  showPosition() {
-    return this.state === State.Demo || this.state === State.Play;
+    this.grid.onToggle(stripIndex, beatIndex, this.noteLoop.progress);
   }
 }
--- src/client/app/shared/grid/grid.ts
+++ src/client/app/shared/grid/grid.ts
@@ -1,12 +1,30 @@
-import {Instrument} from "../instruments/instrument";
+import * as _ from 'lodash';
+import {Instrument} from '../instruments/instrument';
+
+const livePlayWithin: number = 0.3;
+
+enum State {
+  Count,
+  Demo,
+  Play,
+  Victory
+}
 
 export class Grid {
+  state: State;
+  nextState: State = State.Count;
+  round: number = 0;
+  beat: number = 0;
+  lastDelay: number = 0;
+  active: boolean = false;
+  inactiveRounds: number = 0;
+
   numStrips: number;
   numBeats: number;
   instruments: Instrument[];
-  gridState: number[][];
-  goalState: number[][];
-  playedState: number[][];
+  gridValues: number[][];
+  goalValues: number[][];
+  playedValues: number[][];
   numGoalNotes: number;
 
   constructor(instruments: Instrument[], numBeats: number) {
@@ -14,89 +32,131 @@ export class Grid {
     this.numBeats = numBeats;
     this.instruments = instruments;
 
-    this.clearGrid();
+    this.resetStage();
+  }
+
+  resetStage() {
+    this.gridValues = _.times(this.numStrips, () => _.fill(Array(this.numBeats), 0));
     this.clearPlayed();
     this.newGoal(4);
   }
 
-  clearGrid() {
-    this.gridState = [];
-    for (let i = 0; i < this.numStrips; i++) {
-      let strip: number[] = [];
-      for (let j = 0; j < this.numBeats; j++) {
-        strip.push(0);
-      }
-      this.gridState.push(strip);
-    }
-  }
-
   clearPlayed() {
-    this.playedState = [];
-    for (let i = 0; i < this.numStrips; i++) {
-      this.playedState.push([]);
-    }
+    this.playedValues = _.times(this.numStrips, () => []); // TODO: Replace with _.stubArray when @types/lodash updated
   }
 
   newGoal(maxNotes: number) {
     this.numGoalNotes = 0;
-    this.goalState = [];
-    for (let i = 0; i < this.numStrips; i++) {
+    this.goalValues = _.times(this.numStrips, () => {
       let strip: number[] = [];
       for (let j = 0; j < this.numBeats; j++) {
-        let value = Grid.randomInt(1);
+        let value = this.numGoalNotes < maxNotes ? _.random(1) : 0;
         strip.push(value);
         if (value) {
           this.numGoalNotes++;
         }
       }
-      this.goalState.push(strip);
+      return strip;
+    });
+  }
+
+  onTop() {
+    if (_.isEqual(this.playedValues, this.goalValues)) {
+      this.resetStage();
+      this.nextState = State.Victory;
+    } else {
+      this.clearPlayed();
+      if (this.active) {
+        this.inactiveRounds = 0;
+      } else if (this.inactiveRounds >= 3) {
+        this.nextState = State.Demo;
+      }
     }
+
+    this.state = this.nextState;
+    this.active = false;
+    this.beat = 0;
+
+    switch(this.state) {
+      case State.Count:
+      case State.Victory:
+        this.nextState = State.Demo;
+        this.round = 0;
+
+        break;
+      case State.Demo:
+        this.nextState = State.Play;
+        this.inactiveRounds = 0;
+        break;
+      case State.Play:
+        this.round++;
+        if (!this.active) {
+          this.inactiveRounds++;
+        } else {
+          this.inactiveRounds = 0;
+        }
+    }
+  }
+
+  onBeat() {
+    if (this.state === State.Demo) {
+      this.playGoal(this.beat);
+    } else {
+      this.playBeat(this.beat);
+    }
+    this.beat++;
   }
 
   playGoal(beatIndex: number) {
-    for(let i = 0; i < this.numStrips; i++) {
-      if (this.goalState[i][beatIndex]) {
+    _.times(this.numStrips, (i) => {
+      if (this.goalValues[i][beatIndex]) {
         this.instruments[i].play();
       }
-    }
+    });
   }
 
   playBeat(beatIndex: number) {
-    for(let i = 0; i < this.numStrips; i++) {
-      if (this.gridState[i][beatIndex]) {
+    _.times(this.numStrips, (i) => {
+      if (this.gridValues[i][beatIndex]) {
         this.instruments[i].play();
-        this.playedState[i].push(1);
+        this.playedValues[i].push(1);
       } else {
-        this.playedState[i].push(0);
+        this.playedValues[i].push(0);
       }
-    }
+    });
   }
 
-  toggle(stripIndex: number, beatIndex: number, canPlaySound: () => boolean) {
-    this.gridState[stripIndex][beatIndex] =
-      this.gridState[stripIndex][beatIndex] ? 0 : 1;
-    if (this.gridState[stripIndex][beatIndex] && canPlaySound()) {
+  onToggle(stripIndex: number, beatIndex: number, delay: number) {
+    if (this.state === State.Count || this.state === State.Victory) {
+      return;
+    }
+    this.active = true;
+    this.gridValues[stripIndex][beatIndex] =
+      this.gridValues[stripIndex][beatIndex] ? 0 : 1;
+    if (this.gridValues[stripIndex][beatIndex] && this.canPlaySound(beatIndex, delay)) {
       this.instruments[stripIndex].play();
-      this.playedState[stripIndex][beatIndex] = 1;
-    } else if (!this.gridState[stripIndex][beatIndex]) {
-      this.playedState[stripIndex][beatIndex] = 0;
+      this.playedValues[stripIndex][beatIndex] = 1;
     }
   }
 
-  reachedGoal() {
-    for (let i = 0; i < this.numStrips; i++) {
-      for (let j = 0; j < this.numBeats; j++) {
-        if (this.playedState[i][j] != this.goalState[i][j]) {
-          this.clearPlayed();
-          return false;
-        }
-      }
+  canPlaySound(beatIndex: number, delay: number) {
+    if (beatIndex !== this.beat - 1) {
+      this.lastDelay = 0;
+      return false;
     }
-    this.clearPlayed();
-    return true;
+    this.lastDelay = delay;
+    return delay < livePlayWithin;
   }
 
-  static randomInt(upTo: number) {
-    return Math.floor(Math.random() * (upTo + 1));
+  stateName() {
+    return State[this.state];
+  }
+
+  showCount() {
+    return this.state === State.Count || this.state === State.Victory;
+  }
+
+  showPosition() {
+    return this.state === State.Demo || this.state === State.Play;
   }
 }
